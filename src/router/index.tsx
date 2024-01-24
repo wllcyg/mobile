e263286175451e70601be833f9e028eee8bac213
wll_cyg@126.com <wanliang123>
2024-01-24 14:12:29 +0800

    feat:路由变更

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,8 +1,15 @@
-import {lazy, Suspense} from "react";
+import {lazy, LazyExoticComponent, Suspense} from "react";
 import {createBrowserRouter} from 'react-router-dom'
+import {JSX} from "react/jsx-runtime";
+
 const Loading = lazy(() => import(`@/components/loading/index.tsx`));
-export function widthLazyLoad(componentPath: string) {
-    const LazyComponent = lazy(() => import(`../${componentPath}`));
+const cartton = lazy(() => import(`@/pages/cartoon/index`));
+const error = lazy(() => import(`@/pages/error/index.tsx`));
+const home = lazy(() => import(`@/pages/home/index.tsx`))
+const login = lazy(() => import(`@/pages/login/index.tsx`))
+const app = lazy(() => import(`@/App.tsx`));
+export function widthLazyLoad(components: string | LazyExoticComponent<() => JSX.Element>) {
+    const LazyComponent = components;
     const LazeLoadComponent = (props: object) => {
         return (
             <Suspense fallback={<Loading></Loading>}>
@@ -13,11 +20,11 @@ export function widthLazyLoad(componentPath: string) {
     return LazeLoadComponent;
 }
 
-const App = widthLazyLoad('App.tsx');
-const Cartton = widthLazyLoad('pages/cartoon/index.tsx')
-const Error = widthLazyLoad('pages/error/index.tsx')
-const Home = widthLazyLoad('pages/home/index.tsx')
-const Login = widthLazyLoad('pages/login/index.tsx')
+const App = widthLazyLoad(app);
+const Cartton = widthLazyLoad(cartton)
+const Error = widthLazyLoad(error)
+const Home = widthLazyLoad(home)
+const Login = widthLazyLoad(login)
 const router = createBrowserRouter([
     {
         path: '/',
