bbd7eac54791fd32ec47205f048a26f9e4744f8a
wll_cyg@126.com <wanliang123..>
2024-01-25 23:08:08 +0800

    tab

---
+++ src/pages/cartoon/style.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const CartoonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  height: 80%;
+  padding-top: 10%;
+  .image{
+    width: 300px;
+    height: 300px;
+    border-radius: 10px;
+    background-color: rgb(243,232,255);
+  }
+  .option{
+    
+  }
+`;
